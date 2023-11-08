dacdbcefb62c7587acf3d225df8d41721169a4af
Lina Iyer <lina.iyer@linaro.org>
2023-11-08 04:17:06 -0700

    Enable compiling for Windows on ARM64 platform

--- libraries/lib-string-utils/3party/fast_float.h
+++ libraries/lib-string-utils/3party/fast_float.h
@@ -112,7 +112,8 @@ from_chars_result from_chars_advanced(const char *first, const char *last,
        || defined(__MINGW64__)                                          \
        || defined(__s390x__)                                            \
        || (defined(__ppc64__) || defined(__PPC64__) || defined(__ppc64le__) || defined(__PPC64LE__)) \
-       || defined(__EMSCRIPTEN__))
+       || defined(__EMSCRIPTEN__)) \
+       || defined(_M_ARM64)
 #define FASTFLOAT_64BIT
 #elif (defined(__i386) || defined(__i386__) || defined(_M_IX86)   \
      || defined(__arm__) || defined(_M_ARM)                   \
